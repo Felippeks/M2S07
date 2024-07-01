b4f0f12fa310c1e00429752d8642a2a2e3cd438b
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-06-30 23:41:43 -0300

    exercicio 04

--- src/main/java/br/senai/lab365/semana7/service/ConsultaService.java
+++ src/main/java/br/senai/lab365/semana7/service/ConsultaService.java
@@ -1,4 +1,37 @@
-package br.senai.lab365.semana7.service;
+import br.senai.lab365.semana7.entity.Consulta;
+import br.senai.lab365.semana7.repository.ConsultaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class ConsultaService {
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    public Consulta createConsulta(Consulta consulta) {
+        return consultaRepository.save(consulta);
+    }
+
+    public Consulta updateConsulta(Consulta consulta) {
+        return consultaRepository.save(consulta);
+    }
+
+    public void deleteConsulta(Long id) {
+        consultaRepository.deleteById(id);
+    }
+
+    public List<Consulta> getAllConsultas() {
+        return consultaRepository.findAll();
+    }
+
+    public Consulta getConsultaById(Long id) {
+        return consultaRepository.findById(id).orElse(null);
+    }
+
+    public ConsultaService(ConsultaRepository consultaRepository) {
+        this.consultaRepository = consultaRepository;
+    }
 }
--- src/main/java/br/senai/lab365/semana7/service/NutricionistaService.java
+++ src/main/java/br/senai/lab365/semana7/service/NutricionistaService.java
@@ -1,4 +1,36 @@
-package br.senai.lab365.semana7.service;
+import br.senai.lab365.semana7.entity.Nutricionista;
+import br.senai.lab365.semana7.repository.NutricionistaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class NutricionistaService {
+
+    @Autowired
+    private NutricionistaRepository nutricionistaRepository;
+
+    public Nutricionista createNutricionista(Nutricionista nutricionista) {
+        return nutricionistaRepository.save(nutricionista);
+    }
+
+    public Nutricionista updateNutricionista(Nutricionista nutricionista) {
+        return nutricionistaRepository.save(nutricionista);
+    }
+
+    public void deleteNutricionista(Long id) {
+        nutricionistaRepository.deleteById(id);
+    }
+
+    public List<Nutricionista> getAllNutricionistas() {
+        return nutricionistaRepository.findAll();
+    }
+
+    public Nutricionista getNutricionistaById(Long id) {
+        return nutricionistaRepository.findById(id).orElse(null);
+    }
+    public NutricionistaService(NutricionistaRepository nutricionistaRepository) {
+        this.nutricionistaRepository = nutricionistaRepository;
+    }
 }
